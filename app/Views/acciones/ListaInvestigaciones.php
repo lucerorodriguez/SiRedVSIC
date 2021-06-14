fed531b214f543587cf1709b324e02f4588fd525
Lucero <lucerorzbz@gmail.com>
2021-06-14 00:18:47 -0500

    Cambios después de que la cagó Lucero

--- app/Views/acciones/ListaInvestigaciones.php
+++ app/Views/acciones/ListaInvestigaciones.php
@@ -3,25 +3,6 @@
 </br></br></br>
 <div class="container">
 
- <!-- <form action="<?php echo base_url(); ?>/index.php/Investigaciones/busqueda" method="POST" class="form">
-    <div class="form-row align-items-center">
-      <div class="col-sm-3 my-1">
-        <input type="text" name="oracion" value="" class="form-control" placeholder="escribe algo...">
-      </div>
-      <div class="col-auto my-1">
-        <label class="mr-sm-2" for="inlineFormCustomSelect">Filtrar</label>
-        <select class="custom-select mr-sm-2" name="filtro" id="inlineFormCustomSelect">
-          <option selected>Selecciona una opción...</option>
-          <option value="nombre_proyecto">Nombre</option>
-          <option value="objetivos">Objetivos</option>
-          <option value="descripcion">Descripción</option>
-        </select>
-      </div>
-      <div class="col-auto my-1">
-        <button class="btn btn-primary">Buscar</button>
-      </div>
-    </div>
-  </form>-->
   <br> <br>
   <table class="table" id="tb4">
     <thead>
